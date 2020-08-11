19774f30e9c87cc21486c2c08ca24b858868e7f1
shashika1990 <shashika.janith@gmail.com>
2020-08-11 12:31:53 +0530

    Implement the 'Ratings and Reviews' screen UI.

---
+++ app/src/main/java/lk/fmt/findmytutor/activities/RatingsAndReviewsActivity.kt
@@ -0,0 +1,12 @@
+package lk.fmt.findmytutor.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import lk.fmt.findmytutor.R
+
+class RatingsAndReviewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ratings_and_reviews)
+    }
+}
